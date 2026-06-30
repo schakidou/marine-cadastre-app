d5c6b3c6b1b185569a208406692359529cd896c5
schalkidou <schalkidou@gmail.com>
2026-06-30 14:50:42 +0300

    splash window and fix error in widget

--- cdn/5/widgets/conflict-identifier/config.ts
+++
@@ -1,8 +0,0 @@
-import { ImmutableObject } from 'jimu-core'
-
-export interface Config {
-  // The map widget ID that this widget connects to
-  useMapWidgetId: string
-}
-
-export type IMConfig = ImmutableObject<Config>
